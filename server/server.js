a6dd26cd2e28436042e62ecc82c4f882e80a6a3b
ALLAN ROY <aroy33169@gmail.com>
2026-07-12 21:51:20 +0530

    Add HTTP request logger middleware

--- server/server.js
+++ server/server.js
@@ -25,6 +25,11 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 // Initialize Database with promise tracking
 let dbInitialized = false;
 let dbInitError = null;
